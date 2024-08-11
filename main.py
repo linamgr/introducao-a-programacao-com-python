46b844aac4c98cc9ecddfcf51f7b79fb05b44abf
LinaGarces <7254934-LinaGarces@users.noreply.replit.com>
2024-08-11 22:47:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# isso é um comentário de uma linha.
+'''
+Isso é um comentário de várias linhas. 
+Comentários não são executados pelo interpretador de python. 
+Comentários servem para explicar o código.
+'''
+#inicio do programa
+#impressão no terminal da palavra 'Olá Mundo!'
+
+print("Olá mundo!")
+
+#declaramos uma variavel 'nome' e atribuímos a ela o valor 'Maria'
+nome = "Maria"
+#podemos imprimir a mensagem 'Olá Maria' imprimindo o valor da variável 'nome'
+print("Olá " + nome) 
+
+#solicitamos um novo nome e armazenamos o valor digitado na variável 'nome'
+print("Digite um novo nome: ")
+nome = input()
+
+#imprimimos a mensagem 'Olá' seguido do novo nome digitado
+print("Olá " + nome)
+
+#Para executar o programa é só clicar no botão verde Run. Observe as saídas do programa no Terminal (Console).
